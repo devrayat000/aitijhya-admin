ce4dcaa9a4dfe483c92438300da57f262221b9f4
Zul Ikram Musaddik Rayat <98980963+devrayat000@users.noreply.github.com>
2024-05-02 23:56:43 +0600

    code refactor - 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,3 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import { createRequire } from "node:module";
-import CopyWebpackPlugin from "copy-webpack-plugin";
-
-const require = createRequire(import.meta.url);
-
-const cMapsDir = path.join(
-  path.dirname(require.resolve("pdfjs-dist/package.json")),
-  "cmaps"
-);
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -34,31 +20,9 @@ const nextConfig = {
   },
   reactStrictMode: false,
   webpack(config, { webpack }) {
-    config.module.rules.push({
-      test: /\.pdf$/,
-      use: {
-        loader: "file-loader",
-        options: {
-          name: "[path][name].[ext]",
-        },
-      },
-    });
-    config.resolve.alias.canvas = false;
-
     config.plugins.push(
       new webpack.IgnorePlugin({
         resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
-      })
-    );
-
-    config.plugins.push(
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: cMapsDir,
-            to: "cmaps/",
-          },
-        ],
       })
     );
     return config;
