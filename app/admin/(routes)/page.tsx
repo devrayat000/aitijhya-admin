0dfcbc7b45e56c60d933225fb08fc5fa61214031
Zul Ikram Musaddik Rayat <98980963+devrayat000@users.noreply.github.com>
2024-05-31 05:13:22 +0600

    admin stats - lvl1

--- app/admin/(routes)/page.tsx
+++ app/admin/(routes)/page.tsx
@@ -3,6 +3,7 @@ import {
   BookTextIcon,
   NewspaperIcon,
   UsersIcon,
+  MenuIcon,
 } from "lucide-react";
 
 import { Separator } from "@/components/ui/separator";
@@ -13,6 +14,7 @@ import DailyUserChart from "./components/daily-user-count";
 import { getStats } from "@/server/miscellaneous/service/get-stats";
 import { runReport } from "@/server/miscellaneous/service/get-analytics";
 import DailyActiveUserBar from "./components/daily-active-user-count";
+import CountCard from "./components/count-card";
 
 interface DashboardPageProps {
   params: {
@@ -34,50 +36,15 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
         <Heading title="Dashboard" description="Overview of your store" />
         <Separator />
         <div className="grid gap-4 grid-cols-5">
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 flex-nowrap">
-              <CardTitle className="text-sm font-medium flex-1">
-                Total User Count
-              </CardTitle>
-              <UsersIcon className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">{userCount}</div>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-                Total Post Count
-              </CardTitle>
-              <NewspaperIcon className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">{postCount}</div>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-                Total Book Count
-              </CardTitle>
-              <BookTextIcon className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">{bookAuthorCount}</div>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-                Total Chapter Count
-              </CardTitle>
-              <BookOpenTextIcon className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">{chapterCount}</div>
-            </CardContent>
-          </Card>
+          <CountCard name="User" count={userCount} icon={UsersIcon} />
+          <CountCard name="Post" count={postCount} icon={NewspaperIcon} />
+          <CountCard name="Book" count={bookAuthorCount} icon={BookTextIcon} />
+          <CountCard
+            name="Chapter"
+            count={chapterCount}
+            icon={BookOpenTextIcon}
+          />
+          <CountCard name="Subject" count={subjectCount} icon={MenuIcon} />
         </div>
         <div className="grid grid-cols-2 gap-x-4">
           <Card className="col-span-2 lg:col-span-1">
